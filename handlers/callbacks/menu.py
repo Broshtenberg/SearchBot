d5539fb7504bfdac81bc960eb85b7b78fdde5f6b
Victor <broshkin.victor@yandex.ru>
2022-11-03 18:30:17 +0300

    Refactoring

--- handlers/callbacks/menu.py
+++ handlers/callbacks/menu.py
@@ -1,7 +1,7 @@
 from aiogram import Router, types
 from aiogram.filters.text import Text
 from config import bot
-from keyboards.parsing import create_main_menu
+from keyboards.parsing import create_main_menu, create_parse_menu, create_parse_members_menu
 
 menu_router = Router()
 
@@ -12,13 +12,13 @@ async def show_main_menu(callback: types.CallbackQuery):
                                 reply_markup=create_main_menu())
 
 
-@menu_router.callback_query(Text(text="parsing_menu"))
+@menu_router.callback_query(Text(text="parsing"))
 async def show_parsing_menu(callback: types.CallbackQuery):
     await bot.edit_message_text("Меню парсинга", callback.from_user.id, callback.message.message_id,
-                                reply_markup=create_main_menu())
+                                reply_markup=create_parse_menu())
 
 
 @menu_router.callback_query(Text(text="parsing_menu"))
 async def show_parsing_menu(callback: types.CallbackQuery):
     await bot.edit_message_text("Меню парсинга", callback.from_user.id, callback.message.message_id,
-                                reply_markup=create_main_menu())
+                                reply_markup=create_parse_members_menu())
--- keyboards/parsing.py
+++ keyboards/parsing.py
@@ -8,7 +8,7 @@ def create_main_menu():
     keyboard.add(types.InlineKeyboardButton(text="Поиск чатов", callback_data="search"))
     keyboard.add(types.InlineKeyboardButton(text="Парсинг", callback_data="parsing"))
     keyboard.add(types.InlineKeyboardButton(text="Рассылка", callback_data="mailing"))
-    keyboard.add(types.InlineKeyboardButton(text="Ивайтинг", callback_data="mailing"))
+    keyboard.add(types.InlineKeyboardButton(text="Ивайтинг", callback_data="inviting"))
     keyboard.adjust(2)
     return keyboard.as_markup()
 
@@ -16,9 +16,9 @@ def create_main_menu():
 # Parsing Menu created
 def create_parse_menu():
     keyboard = InlineKeyboardBuilder()
-    keyboard.add(types.InlineKeyboardButton(text="Участников чата/группы", callback_data="parsing"))
-    keyboard.add(types.InlineKeyboardButton(text="Участников обсуждений на канале", callback_data="mailing"))
-    keyboard.add(types.InlineKeyboardButton(text="Назад в Главное меню", callback_data="search"))
+    keyboard.add(types.InlineKeyboardButton(text="Участников чата/группы", callback_data="chat_members"))
+    keyboard.add(types.InlineKeyboardButton(text="Участников обсуждений на канале", callback_data="discussion_members"))
+    keyboard.add(types.InlineKeyboardButton(text="Назад в Главное меню", callback_data="main_menu"))
     keyboard.adjust(1)
     return keyboard.as_markup()
 
@@ -26,11 +26,11 @@ def create_parse_menu():
 # Parsing members Menu created
 def create_parse_members_menu():
     keyboard = InlineKeyboardBuilder()
-    keyboard.add(types.InlineKeyboardButton(text="Всех", callback_data="parsing"))
-    keyboard.add(types.InlineKeyboardButton(text="Активных", callback_data="mailing"))
-    keyboard.add(types.InlineKeyboardButton(text="Авторов сообщений", callback_data="search"))
-    keyboard.add(types.InlineKeyboardButton(text="Админов", callback_data="search"))
-    keyboard.add(types.InlineKeyboardButton(text="Назад в Главное меню", callback_data="search"))
+    keyboard.add(types.InlineKeyboardButton(text="Всех", callback_data="all_members"))
+    keyboard.add(types.InlineKeyboardButton(text="Активных", callback_data="active_members"))
+    keyboard.add(types.InlineKeyboardButton(text="Авторов сообщений", callback_data="authors"))
+    keyboard.add(types.InlineKeyboardButton(text="Админов", callback_data="admins"))
+    keyboard.add(types.InlineKeyboardButton(text="Назад в Главное меню", callback_data="main_menu"))
     keyboard.adjust(2)
     return keyboard.as_markup()
 
